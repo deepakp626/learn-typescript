33106141126b763958a874259a83baa851f53454
deepakprajapat62600 <deepakprajapat62600@gmail.com>
2025-09-01 19:34:59 +0000

    learn data type and folder structure

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+// const a = "Nice"
+// console.log(a)
+// const b = 55
+// console.log(b)
+// let a = 22
+// let nambo = "deep";
+// how to declare datatype in typescript
+// let a:string = "name Here";
+// let nambo:number = 343;
+// let check:boolean
+const func = (n, m) => {
+};
+export {};
+//# sourceMappingURL=index.js.map
