bc86188acc5fef285cbb8945f0d71b6d1b3d1424
deepakprajapat62600 <deepakprajapat62600@gmail.com>
2025-09-07 20:09:12 +0000

    learn typescript Array

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,35 @@
 // let a:string = "name Here";
 // let nambo:number = 343;
 // let check:boolean
-const func = (n, m) => {
-};
+// declaration syntax
+// let nambo = <string>"deep"  // right syntax
+// let strbo = <number>343  // right syntax
+// console.log(nambo,strbo)
+//  -----------  Array in Typrscript ------------ 
+// type Username = string | number;
+// let a:Username = 3434
+// type Username = (n: number, m: string) => string;
+// const func: Username = (n, m) => {
+//     console.log(n, m)
+//     return n + m;
+// }
+// ------------ Array ------------
+// const arr = [12,23,44,65,45]
+const arr = [23, 34, 45, 56, 67, 78, 89, 90]; // only number is allow to store in array
+const strr = ["hii", "me", "ok"]; // only string is allow in array
 export {};
+// --------- Array with Generics -----------
+// const arr2:Array<string> = ["hii","me","ok"]
+// const arr3:Array<number> = [23,34,45,56,67,78,89,90]
+// Similar
+// const arrtest: Array<string> = new Array(20)
+// store data in two types of data
+// const arr5: Array<string | number> = [23,23,"hii",34,true]
+// const arr6:Array<string> = ["mahadave","jai","hii","ok,","buy"];
+// arr6.forEach((item,index) => {
+//     console.log(item,index)
+// })
+// Tuple -- fix type array
+// const arr8:[number,number,number] = [23,34,45,44]
+// console.log(arr8)
 //# sourceMappingURL=index.js.map
