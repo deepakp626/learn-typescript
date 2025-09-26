bd99f4e0d3155c29aff5a53da284df5a1115e4ef
deepakprajapat62600 <deepakprajapat62600@gmail.com>
2025-09-26 22:33:10 +0000

    Generics Crash course

--- dist/index.js
+++ dist/index.js
@@ -1,59 +1,5 @@
 // const a = "Nice"
 // console.log(a)
-// const b = 55
-// console.log(b)
-// let a = 22
-// let nambo = "deep";
-// how to declare datatype in typescript
-// let a:string = "name Here";
-// let nambo:number = 343;
-// let check:boolean
-// declaration syntax
-// let nambo = <string>"deep"  // right syntax
-// let strbo = <number>343  // right syntax
-// console.log(nambo,strbo)
-//  -----------  Array in Typrscript ------------ 
-// type Username = string | number;
-// let a:Username = 3434
-// type Username = (n: number, m: string) => string;
-// const func: Username = (n, m) => {
-//     console.log(n, m)
-//     return n + m;
-// }
-// ------------ Array ------------
-// const arr = [12,23,44,65,45]
-const arr = [23, 34, 45, 56, 67, 78, 89, 90]; // only number is allow to store in array
-const strr = ["hii", "me", "ok"]; // only string is allow in array
-const gigi = {
-    height: 342,
-    weight: 33,
-    scoler: true,
-    func: (n, m) => {
-        console.log("n*m =", n * m);
-    }
-};
-const kendle = {
-    height: 342,
-    weight: 33,
-    scoler: true,
-    func: (n, m) => {
-        console.log("n*m =", n * m);
-    }
-};
-kendle.func(10, 20);
-const obg = {
-    height: 342,
-    weight: 33,
-    gender: true
-};
-const obj2 = {
-    height: 342,
-    weight: 33
-};
-const func = (n, m) => {
-    return n * m;
-};
-console.log(func(10, 20));
 const func3 = (n, m, l = 10) => {
     return n * m * l;
 };
@@ -203,15 +149,292 @@ btn.onclick;
 // method 5
 const img = document.getElementById("myimg");
 img.src;
-const myObject = {
-    name: "abhi",
-    email: "abhi@gmail.com"
-};
-// const key = "name"
-// let key:keyof Person = "name"
-let key = "name";
-myObject[key];
-// ------ 4 -- ager person interface na hoth typeof ka throw 
-myObject[key];
 export {};
+// ------------ example for DOM manuculation --------------
+// method 6
+// specific query selector ma type assertion define karna nahe hotha hai
+// const img2 = document.querySelector("img")!
+// img2.src
+// const form = document.getElementById("myfomr") as HTMLFormElement
+// const myinput = document.querySelector("myform  > input") as HTMLInputElement
+// const mybtn = document.querySelector("myform  > button") as HTMLButtonElement
+// form.onsubmit = function (e: SubmitEvent) {
+//     e.preventDefault();
+//     let data = Number(myinput.value);
+//     console.log(data);
+//     console.log(typeof data);
+//     const h2 = document.createElement("h2");
+//     h2.textContent = String(data + 20);
+//     document.body.append(h2);
+// };
+// -------------  Index Signature ---------------
+// interface Person {
+//     [key: string]: string | number
+// }
+// interface Person {
+//     name:string,
+//     email:string
+// }
+// const myObj: Person = {
+//     name: "Jai",
+//     email: "test@gmail.com"
+// }
+// const getName = ()=> {
+//     return myObj["name"];
+// }
+// const getEmail = () => {
+//     return myObj["email"];
+// }
+// upper ka both function ko merge kara hai 
+// -------  1 -- dynamicaly define karna hai tho
+// const getMyData = (key: string) => {
+//     // myObj["name"]
+//     // myObj["email"]
+//     return myObj[key]
+// }
+// -------- 2 -- ager jo key object ma hai vo he lena hai tho
+// const getSpeData = (key : "name" | "email") => {
+//     return myObj[key]
+// }
+// getSpeData("name")
+// ----- 3 -- keyof ka throw dynamically define karna 
+// interface Person {
+//     name : string;
+//     email: string
+// }
+// const myObject:Person = {
+//     name:"abhi",
+//     email:"abhi@gmail.com"
+// }
+// // const key = "name"
+// // let key:keyof Person = "name"
+// let key = "name" 
+// myObject[key as keyof Person]
+// // ------ 4 -- ager person interface na hoth typeof ka throw 
+// myObject[key as keyof typeof myObject]
+// --------------  Type Utility ----------------
+// Partial<Type>
+// Required<Type>
+// Readonly<Type>
+// Record<keys,type>
+// Pick<Type,keys>
+// Omit<Type,keys>
+// Exclude<Type,ExcludeType>
+// Extract<Type,ExtractType>
+// NonNullable<Type>
+// Parameters<Type>
+// ConstructorParameters<Type>
+// ReturnType<Type>
+// InstanceType<Type>
+// ThisParameterType<Type>
+// OmitThisParameter<Type>
+// ThisType<Type>
+// ------------------
+// Partial<Type>
+// type User = {
+//     name: string,
+//     email:string
+// }
+// type User2 = Partial<User>  /// user2 ke dono field optional hogai hai
+// Required<Type>
+// type User = {
+//     name?: string,
+//     email:string
+// }
+// type User3 = Required<User>  /// user3 ke dono field required hogai hai
+// Readonly<Type>
+// type User = {
+//     name: string,
+//     email:string
+// }
+// type User4 = Readonly<User>  /// user4 ke dono field read only hogai hai
+// Record<keys,type>
+// type user  = {
+//       name: string,
+//       email:string
+// }
+// type User2 = Record<"name" | "email" | "gerder",string>
+// // Example
+// interface UserInfo {
+//     age:Number
+// }
+// type UserName = "john" | "levl" | "elon" | "jack"
+// const users:Record<UserName,UserInfo> = {
+//     john:{age:20},
+//     levl:{age:30},
+//     elon:{age:40},
+//     jack:{age:50}
+// }
+// --------- Pick<Type,keys> -------------
+// interface OrderInfo {
+//     readonly id :string;
+//     user:string;
+//     city:string;
+//     state:string;
+//     country:string;
+//     status:string;
+// }
+// type ShippingInfo   = Pick<OrderInfo,"city"|"state"|"country">
+//------- Omit<Type,keys> -------------
+// interface ShippingInfo {
+//     city:string;
+//     state:string;
+//     country:string;
+// }
+// type random = Omit<ShippingInfo,"country">
+// Exclude<Type,ExcludeType>
+// type MyUnion = string | number | boolean
+// type random = Exclude<MyUnion,boolean>
+// Extract<Type,ExtractType>
+// type MyUnion = string | number | boolean
+// type random = Extract<MyUnion,boolean>
+// ---------->>>> NonNullable<Type>
+// type MyUnion = string | number | boolean | null | undefined
+// type random = NonNullable<MyUnion>
+// // ---------->>>> Parameters<Type>
+// const myFunc = (a:number,b:number)=>{
+//     console.log(a+b)
+// }
+// type random = Parameters<typeof myFunc>
+// ConstructorParameters<Type>
+// class SampleClass {
+//     constructor(public s:string,public t:string){
+//         this.s = s
+//         this.t = t
+//     }
+// }
+// type random = ConstructorParameters<typeof SampleClass>
+// ReturnType<Type>
+// const myFunc = (a:number,b:number):string =>{
+//     return String(a+b)
+// }
+// type random = ReturnType<typeof myFunc>
+// InstanceType<Type>
+// class SampleClass {
+//     constructor(public s:string,public t:string){
+//         this.s = s
+//         this.t = t
+//     }
+// }
+// type Random = InstanceType<typeof SampleClass>
+// //  user random ka throw bana hai tho saraparameter hona chahiya 
+// // random / SampleClass ka
+// const User:Random = {
+//     s:"jai",
+//     t:"jai"
+// } 
+// -------------------  Generics in TS -------------------- 
+// Generics
+///  typescript ma any ko avoid karna hai 
+/// ager any ko use karega tho typescript ka kam he kya
+// const func = (n:any):any =>{
+//     return n
+// }
+// const ans =func(20)
+// const ans2 =func("20")
+// const ans3 =func(true)
+// ----------- Generics Syntax -------------------
+// const func = <CustomType>(n:CustomType):CustomType =>{
+//     return n
+// }
+// const ans =func(20)
+// const ans2 =func("20")
+// const ans3 =func(true)
+// ----------- code example
+// const func = <T>(n:T):T =>{
+//     return n
+// }
+// const ans =func(20)
+// const ans2 =func("20")
+// const ans3 =func(true)
+// --------- Generic With custom datatype --------
+// type Person = {
+//     name :string,
+//     age:number
+// }
+// const func = <T>(n:T):T =>{
+//     return n
+// }
+// const Person1:Person = {
+//     name:"abhi",
+//     age:24
+// }
+// //  one syntax
+// // const ans = func(Person1)
+// // two syntax
+// const ans = func<Person>(Person1)
+// ------- Generic ka use array ma
+// const arr:number[] = []
+// const arr2:Array<number> = []
+// -------- Generic ma return type Object ---------
+// ------ method - 1
+// const func = <T,U>(n:T,o:U) :object=>{
+//     return {n,o}
+// }
+// const ans = func<number,string>(20,'hii')
+// ans.   // dot sa ans ka n,o ko assecc nahe kar saktha hai
+// // ------ method - 2
+// const func = <T,U>(n:T,o:U) :{n:T,o:U}=>{
+//     return {n,o}
+// }
+// const ans = func<number,string>(20,'hii')
+// ans.n   // dot sa ans variable ko assecc kar saktha hai
+// ------------ method 3 
+// const func = <T,U extends T>(n:T,o:U) :{n:T,o:U}=>{
+//     return {n,o}
+// }
+// const ans = func<number,number>(20,23)
+///// ------------ method 4
+// type Person = {
+//     name:string,
+//     age:number
+// }
+// type Person2 = {
+//     name:string,
+//     age:number,
+//     email:string
+// }
+// const func = <T,U extends T>(n:T,o:U) :{n:T,o:U}=>{
+//     return {n,o}
+// }
+// const user:Person = {
+//     name:"jai",
+//     age:23
+// }
+// const user2:Person2 = {
+//     name:"jai",
+//     age:23,
+//     email:"jai@gmail.com"
+// }
+// const ans = func<Person,Person2>(user,user2)
+//////  ------------ Method 5
+// type Person = {
+//     name:string,
+//     age:number
+// }
+// const users:Person[] = [
+//     {
+//         name:"jai",
+//         age:23
+//     },
+//     {
+//         name:"Nahar",
+//         age:23
+//     },
+//     {
+//         name:"hai",
+//         age:25
+//     },
+//     {
+//         name:"new",
+//         age:25
+//     }
+// ]
+// const filterByPeople = <T,key extends keyof T>(arr:T[],property:key,value:T[key]):T[] =>{
+//    return arr.filter( (item:any) => item[property] === value)
+// }
+// const filterPeopleByName = filterByPeople(users,"name","jai")
+// console.log(filterPeopleByName)
+// const filterPeopleByAge = filterByPeople(users,"age",25)
+// console.log(filterPeopleByAge)
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -38,9 +38,9 @@
 // ------------ Array ------------
 
 // const arr = [12,23,44,65,45]
-const arr: number[] = [23, 34, 45, 56, 67, 78, 89, 90] // only number is allow to store in array
+// const arr: number[] = [23, 34, 45, 56, 67, 78, 89, 90] // only number is allow to store in array
 
-const strr: string[] = ["hii", "me", "ok"]  // only string is allow in array
+// const strr: string[] = ["hii", "me", "ok"]  // only string is allow in array
 
 // --------- Array with Generics -----------
 
@@ -75,61 +75,61 @@ const strr: string[] = ["hii", "me", "ok"]  // only string is allow in array
 // }
 
 
-// interface
-interface obj {
-    height: number,
-    weight: number,
-    gender?: boolean
-}
+// -------------- interface
+// interface obj {
+//     height: number,
+//     weight: number,
+//     gender?: boolean
+// }
 
-type FuncType = (n: number, m: number) => void
+// type FuncType = (n: number, m: number) => void
 
-interface newObj extends obj {
-    scoler: boolean,
-    func: FuncType;
-}
+// interface newObj extends obj {
+//     scoler: boolean,
+//     func: FuncType;
+// }
 
-const gigi: newObj = {
-    height: 342,
-    weight: 33,
-    scoler: true,
-    func: (n, m) => {
-        console.log("n*m =", n * m)
-    }
-}
+// const gigi: newObj = {
+//     height: 342,
+//     weight: 33,
+//     scoler: true,
+//     func: (n, m) => {
+//         console.log("n*m =", n * m)
+//     }
+// }
 
-const kendle: newObj = {
-    height: 342,
-    weight: 33,
-    scoler: true,
-    func: (n, m) => {
-        console.log("n*m =", n * m)
-    }
-}
+// const kendle: newObj = {
+//     height: 342,
+//     weight: 33,
+//     scoler: true,
+//     func: (n, m) => {
+//         console.log("n*m =", n * m)
+//     }
+// }
 
-kendle.func(10, 20)
+// kendle.func(10, 20)
 
-const obg: obj = {
-    height: 342,
-    weight: 33,
-    gender: true
-}
+// const obg: obj = {
+//     height: 342,
+//     weight: 33,
+//     gender: true
+// }
 
-const obj2: obj = {
-    height: 342,
-    weight: 33
-}
+// const obj2: obj = {
+//     height: 342,
+//     weight: 33
+// }
 
 
 // ----------- function in typescript -----------------
 
-type Functype = (n: number, m: number) => number
+// type Functype = (n: number, m: number) => number
 
-const func: Functype = (n, m) => {
-    return n * m
-}
+// const func: Functype = (n, m) => {
+//     return n * m
+// }
 
-console.log(func(10, 20))
+// console.log(func(10, 20))
 
 
 // ---- Optional parameter banana hai tho
@@ -504,34 +504,314 @@ img.src
 
 // ----- 3 -- keyof ka throw dynamically define karna 
 
-interface Person {
-    name : string;
-    email: string
-}
+// interface Person {
+//     name : string;
+//     email: string
+// }
 
-const myObject:Person = {
-    name:"abhi",
-    email:"abhi@gmail.com"
-}
+// const myObject:Person = {
+//     name:"abhi",
+//     email:"abhi@gmail.com"
+// }
 
-// const key = "name"
-// let key:keyof Person = "name"
-let key = "name" 
+// // const key = "name"
+// // let key:keyof Person = "name"
+// let key = "name" 
 
-myObject[key as keyof Person]
+// myObject[key as keyof Person]
 
-// ------ 4 -- ager person interface na hoth typeof ka throw 
+// // ------ 4 -- ager person interface na hoth typeof ka throw 
 
-myObject[key as keyof typeof myObject]
-
-
+// myObject[key as keyof typeof myObject]
 
 
 
+// --------------  Type Utility ----------------
+
+// Partial<Type>
+// Required<Type>
+// Readonly<Type>
+// Record<keys,type>
+// Pick<Type,keys>
+// Omit<Type,keys>
+// Exclude<Type,ExcludeType>
+// Extract<Type,ExtractType>
+// NonNullable<Type>
+// Parameters<Type>
+// ConstructorParameters<Type>
+// ReturnType<Type>
+// InstanceType<Type>
+// ThisParameterType<Type>
+// OmitThisParameter<Type>
+// ThisType<Type>
+
+// ------------------
+
+// Partial<Type>
+// type User = {
+//     name: string,
+//     email:string
+// }
+
+// type User2 = Partial<User>  /// user2 ke dono field optional hogai hai
+
+// Required<Type>
+// type User = {
+//     name?: string,
+//     email:string
+// }
+
+// type User3 = Required<User>  /// user3 ke dono field required hogai hai
+
+
+// Readonly<Type>
+// type User = {
+//     name: string,
+//     email:string
+// }
+
+// type User4 = Readonly<User>  /// user4 ke dono field read only hogai hai
+
+
+// Record<keys,type>
+// type user  = {
+//       name: string,
+//       email:string
+// }
+// type User2 = Record<"name" | "email" | "gerder",string>
+
+// // Example
+// interface UserInfo {
+//     age:Number
+// }
+
+// type UserName = "john" | "levl" | "elon" | "jack"
+
+// const users:Record<UserName,UserInfo> = {
+//     john:{age:20},
+//     levl:{age:30},
+//     elon:{age:40},
+//     jack:{age:50}
+// }
+
+
+// --------- Pick<Type,keys> -------------
+// interface OrderInfo {
+//     readonly id :string;
+//     user:string;
+//     city:string;
+//     state:string;
+//     country:string;
+//     status:string;
+// }
+
+// type ShippingInfo   = Pick<OrderInfo,"city"|"state"|"country">
 
 
 
+//------- Omit<Type,keys> -------------
+// interface ShippingInfo {
+//     city:string;
+//     state:string;
+//     country:string;
+// }
+
+// type random = Omit<ShippingInfo,"country">
+
+// Exclude<Type,ExcludeType>
+// type MyUnion = string | number | boolean
+// type random = Exclude<MyUnion,boolean>
+
+
+// Extract<Type,ExtractType>
+// type MyUnion = string | number | boolean
+// type random = Extract<MyUnion,boolean>
+
+
+// ---------->>>> NonNullable<Type>
+// type MyUnion = string | number | boolean | null | undefined
+// type random = NonNullable<MyUnion>
+
+
+// // ---------->>>> Parameters<Type>
+// const myFunc = (a:number,b:number)=>{
+//     console.log(a+b)
+// }
+// type random = Parameters<typeof myFunc>
+
+
+// ConstructorParameters<Type>
+// class SampleClass {
+//     constructor(public s:string,public t:string){
+//         this.s = s
+//         this.t = t
+//     }
+// }
+// type random = ConstructorParameters<typeof SampleClass>
+
+
+// ReturnType<Type>
+// const myFunc = (a:number,b:number):string =>{
+//     return String(a+b)
+// }
+// type random = ReturnType<typeof myFunc>
+
+
+// InstanceType<Type>
+// class SampleClass {
+//     constructor(public s:string,public t:string){
+//         this.s = s
+//         this.t = t
+//     }
+// }
+// type Random = InstanceType<typeof SampleClass>
+
+// //  user random ka throw bana hai tho saraparameter hona chahiya 
+// // random / SampleClass ka
+// const User:Random = {
+//     s:"jai",
+//     t:"jai"
+// } 
+
+
+// -------------------  Generics in TS -------------------- 
+// Generics
+
+///  typescript ma any ko avoid karna hai 
+/// ager any ko use karega tho typescript ka kam he kya
+// const func = (n:any):any =>{
+//     return n
+// }
+// const ans =func(20)
+// const ans2 =func("20")
+// const ans3 =func(true)
+
+
+// ----------- Generics Syntax -------------------
+// const func = <CustomType>(n:CustomType):CustomType =>{
+//     return n
+// }
+// const ans =func(20)
+// const ans2 =func("20")
+// const ans3 =func(true)
+
+
+// ----------- code example
+// const func = <T>(n:T):T =>{
+//     return n
+// }
+// const ans =func(20)
+// const ans2 =func("20")
+// const ans3 =func(true)
+
+
+// --------- Generic With custom datatype --------
+
+// type Person = {
+//     name :string,
+//     age:number
+// }
+// const func = <T>(n:T):T =>{
+//     return n
+// }
+// const Person1:Person = {
+//     name:"abhi",
+//     age:24
+// }
+
+// //  one syntax
+// // const ans = func(Person1)
+
+// // two syntax
+// const ans = func<Person>(Person1)
+
+
+// ------- Generic ka use array ma
+// const arr:number[] = []
+// const arr2:Array<number> = []
 
 
 
+// -------- Generic ma return type Object ---------
+// ------ method - 1
+// const func = <T,U>(n:T,o:U) :object=>{
+//     return {n,o}
+// }
+// const ans = func<number,string>(20,'hii')
+// ans.   // dot sa ans ka n,o ko assecc nahe kar saktha hai
 
+
+// // ------ method - 2
+// const func = <T,U>(n:T,o:U) :{n:T,o:U}=>{
+//     return {n,o}
+// }
+// const ans = func<number,string>(20,'hii')
+// ans.n   // dot sa ans variable ko assecc kar saktha hai
+
+// ------------ method 3 
+// const func = <T,U extends T>(n:T,o:U) :{n:T,o:U}=>{
+//     return {n,o}
+// }
+// const ans = func<number,number>(20,23)
+
+
+///// ------------ method 4
+// type Person = {
+//     name:string,
+//     age:number
+// }
+// type Person2 = {
+//     name:string,
+//     age:number,
+//     email:string
+// }
+// const func = <T,U extends T>(n:T,o:U) :{n:T,o:U}=>{
+//     return {n,o}
+// }
+// const user:Person = {
+//     name:"jai",
+//     age:23
+// }
+// const user2:Person2 = {
+//     name:"jai",
+//     age:23,
+//     email:"jai@gmail.com"
+// }
+// const ans = func<Person,Person2>(user,user2)
+
+
+
+//////  ------------ Method 5
+// type Person = {
+//     name:string,
+//     age:number
+// }
+
+// const users:Person[] = [
+//     {
+//         name:"jai",
+//         age:23
+//     },
+//     {
+//         name:"Nahar",
+//         age:23
+//     },
+//     {
+//         name:"hai",
+//         age:25
+//     },
+//     {
+//         name:"new",
+//         age:25
+//     }
+// ]
+
+// const filterByPeople = <T,key extends keyof T>(arr:T[],property:key,value:T[key]):T[] =>{
+
+//    return arr.filter( (item:any) => item[property] === value)
+// }
+
+// const filterPeopleByName = filterByPeople(users,"name","jai")
+// console.log(filterPeopleByName)
+// const filterPeopleByAge = filterByPeople(users,"age",25)
+// console.log(filterPeopleByAge)
